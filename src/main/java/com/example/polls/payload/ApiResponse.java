310f717ff3e11ad40a78ba025ed96b2e5aa05963
anoop abbot <anoop.abbot007@gmail.com>
2020-03-27 04:56:51 -0700

    added new project based on spring sercurity and jwt with mysql

---
+++ src/main/java/com/example/polls/payload/ApiResponse.java
@@ -0,0 +1,30 @@
+package com.example.polls.payload;
+
+/**
+ * Created by //Anoop Abbot on 27/03/2020.
+ */
+public class ApiResponse {
+    private Boolean success;
+    private String message;
+
+    public ApiResponse(Boolean success, String message) {
+        this.success = success;
+        this.message = message;
+    }
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
